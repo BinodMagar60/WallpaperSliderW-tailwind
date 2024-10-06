f8b5335ac899dc711542256cf1752222e0ef78a3
unknown <kauchabinod88@gmail.com>
2024-10-06 09:01:29 +0545

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState, useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const refs = useRef([]); 
+  const [translateX, setTranslateX] = useState(0);
+  const refs = useRef([]);
 
   const left = () => {
-    refs.current.forEach(index => {
-      index.style.transform = 'translateX(-242px)'; 
-    })
+    
+    if (translateX > -(refs.current.length - 1) * 242) {
+      setTranslateX((prev) => prev - 242);
+    }
+  };
+
+  const right = () => {
+   
+    if (translateX < 0) {
+      setTranslateX((prev) => prev + 242);
+    }
   };
 
   const mydata = [
@@ -31,23 +39,26 @@ function App() {
             >
               <button
                 className="border-2 border-gray-700 p-2 rounded-full hover:text-white hover:bg-black hover:fill-white transition ease-in-out duration-300"
-                onClick={left} 
+                onClick={left}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="w-5 h-5"
                   viewBox="0 0 256 512"
                 >
-                  <path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z" />
+                  <path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6v256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z" />
                 </svg>
               </button>
-              <button className="border-2 border-gray-700 p-2 rounded-full hover:text-white hover:bg-black hover:fill-white transition ease-in-out duration-300">
+              <button
+                className="border-2 border-gray-700 p-2 rounded-full hover:text-white hover:bg-black hover:fill-white transition ease-in-out duration-300"
+                onClick={right}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="w-5 h-5"
                   viewBox="0 0 256 512"
                 >
-                  <path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9-7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z" />
+                  <path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6v256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z" />
                 </svg>
               </button>
             </div>
@@ -56,10 +67,11 @@ function App() {
               {mydata.map((item, index) => (
                 <img
                   key={index}
-                  ref={(el) => (refs.current[index] = el)} // Store each image ref in the refs array
+                  ref={(el) => (refs.current[index] = el)} 
                   src={item.url}
                   alt={`Photo no.` + item.id}
-                  className="w-[220px]"
+                  className="w-[220px] transition-transform duration-500 ease-in-out" 
+                  style={{ transform: `translateX(${translateX}px)` }}
                 />
               ))}
             </div>
